bebf3498505534cad22ac7557b5cd3036fe74d3e
Jenny Yang <jennyyang01@gmail.com>
2020-08-22 23:54:25 -0400

    Create database

--- db/db.js
+++ db/db.js
@@ -1,3 +1,27 @@
 import {SQLite} from 'expo-sqlite';
 
 const db = SQLite.openDatabase("daytrip.db");
+
+const addDataToDb = () => {
+
+    db.transaction(tx => {
+  
+      tx.executeSql(
+        "create table if not exists DataTable (id integer primary key not null, column_1 int, column_2 int, column_3 text);",
+        []
+      );
+  
+      tx.executeSql(
+        "insert into DataTable (column_1, column_2, column_3) values (?, ?, ?)",
+        [argument_1, argument_2, argument_3]
+      );
+  
+      tx.executeSql(
+        "select * from scoreData",
+        [],
+        (_, { rows: { _array } }) => setScores(_array),
+        () => console.log("error fetching")
+      );
+  
+    });
+};
